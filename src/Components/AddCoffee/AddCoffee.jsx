4cf163f6a24b411d77ad36589d878797951f01f7
MOHAMMAD KHUBAIB SARKER <mohammad@kali.kali>
2024-04-08 12:30:26 +0600

    create form

--- src/Components/AddCoffee/AddCoffee.jsx
+++ src/Components/AddCoffee/AddCoffee.jsx
@@ -1,7 +1,36 @@
 const AddCoffee = () => {
     return (
         <div>
-            <h1>create user</h1>
+            <h1>Add a coffee</h1>
+            <form>
+                <div className="flex">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input
+                                className="input input-bordered "
+                                placeholder="Coffee name"
+                                name="coffeeName"
+                            />
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">
+                                Available Quantity
+                            </span>
+                        </label>
+                        <label className="input-group">
+                            <input
+                                className="input input-bordered "
+                                placeholder="available quantity"
+                            />
+                        </label>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
